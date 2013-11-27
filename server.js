7bed15a728a1dc37a679f86842b5a47c3a343d03
Nils Winkler <nils@nilswinkler.com>
2013-11-27 16:43:40 +0100

    Moving to model supported by the angular-fullstack yeoman generator.
    Project structure changed to fullstack model.

---
+++ server.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+
+var app = express();
+
+// Connect to database
+var db = require('./lib/db/mongo');
+
+// Bootstrap models
+var modelsPath = path.join(__dirname, 'lib/models');
+fs.readdirSync(modelsPath).forEach(function (file) {
+  require(modelsPath + '/' + file);
+});
+
+// Express Configuration
+app.configure(function(){
+  app.set('views', path.join(__dirname, 'app/views'));
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+});
+
+// development only
+app.configure('development', function(){
+  app.use(require('connect-livereload')());
+  app.use(express.static(path.join(__dirname, '.tmp')));
+  app.use(require('less-middleware')({ src: path.join(__dirname, 'app') }));
+  app.use(express.static(path.join(__dirname, 'app')));
+  app.use(express.errorHandler());
+});
+
+app.configure('production', function(){
+  app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')));
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var routes = require('./lib/controllers');
+var shortcut = require('./lib/controllers/shortcut');
+
+app.get('/', routes.index);
+app.get('/:id', shortcut.get);
+app.get('/shortcut/:id', shortcut.retrieve);
+app.post('/shortcut', shortcut.post);
+
+// Start server
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
